9e4581f85c3448bf4a74a571e15aae6d5e727d82
Sergio <sergio@rendergon.com>
2024-09-05 20:02:06 +0100

    stump wip

--- CNN-share-price-prediction/X-Channel-Images-Project/helper_functions_dir/embed_fluss.py
+++ CNN-share-price-prediction/X-Channel-Images-Project/helper_functions_dir/embed_fluss.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import stumpy
 import yfinance as yf
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
 
 import importlib as importlib
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
@@ -18,7 +19,6 @@ if cuda.is_available():
     print("Found cuda for numba")
 else:
     print("no cuda for numba")
-
 
 dataset_df = yf.download(Parameters.train_stock_ticker, start=Parameters.start_date, end=Parameters.end_date, interval='1d')
 dataset_df = dataset_df.dropna()
@@ -34,18 +34,38 @@ else:
 
 dataset_df = dataset_df.set_index('Date')
 
+print(dataset_df)
+
+scaler = MinMaxScaler(Parameters.min_max_scaler_feature_range)
+print("BEFORE SCALER")
+#scaled_data_f32 = Parameters.scaler.fit_transform(dataset_df['Close'].values.reshape(-1, 1))
+vals = dataset_df['Close'].values.reshape(-1, 1)
+print("type",type(vals))
+scaled_data = scaler.fit_transform(vals).reshape(-1,)
+print("AFTER SCALER len",len(scaled_data))
+
 #Matrix profile and window size
 window_size = 32
-matrix_profile = stumpy.gpu_stump(dataset_df['Close'], m=window_size)
+# matrix_profile 4 index result meaning:
+#[distance, index_of_nearest_neighbor, motif_index, motif_length]
+# Distance: Euclidean distance between the current subsequence and its nearest neighbor in the time series
+# Index: The index of the nearest neighbor subsequence in the time series
+# Motif Index: The index of the current subsequence in the time series
+# Motif Length: The length of the subsequence
 
-print(matrix_profile)
-
+print("len reshaped",len(scaled_data))
+matrix_profile = stumpy.gpu_stump(scaled_data, m=window_size)
+print("matrix values",matrix_profile.P_,"matrix indices", matrix_profile.I_)
+matrix_profile_df = pd.DataFrame(matrix_profile, columns=['profile', 'profile index', 'left profile index', 'right profile index'])
+print(matrix_profile_df)                                                          
 #identify change points with FLUSS
 #spaced out segments by min excl_factor* window_size
-flux = stumpy.fluss(matrix_profile[:, 1], L=window_size, n_regimes=5, excl_factor=5)
-
-# Find the change points (discontinuities in semantic state)
-change_points = np.argwhere(np.diff(flux['T']) < 0).flatten()
+correct_arc_curve, regime_locations = stumpy.fluss(matrix_profile[:, 1], L=window_size, n_regimes=5, excl_factor=5)
+print(correct_arc_curve)
+print("---")
+print(regime_locations)
+#Find the change points (discontinuities in semantic state)
+change_points = np.argwhere(np.diff(correct_arc_curve['T']) < 0).flatten()
 
 # Visualize the change points on the time series
 plt.figure(figsize=(12, 6))
